0ab5be05b14889aba3c58df948f3985d731d034c
juliannne <jplegado@calpoly.edu>
2024-09-26 12:37:10 -0700

    IA 1: First React App final

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 import MyApp from "./MyApp";
 import "./main.css";
+import Form from "./Form";
 
 // Create the container
 const container = document.getElementById("root");
