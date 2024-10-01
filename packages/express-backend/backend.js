5071c36c6cb230251c709361a540ab00d9b684d2
juliannne <jplegado@calpoly.edu>
2024-10-01 10:49:02 -0700

    Add /users/:id route to retrieve user by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+
+
 const users = {
   users_list: [
     {
@@ -33,6 +36,10 @@ const users = {
   ]
 };
 
+const findUserById = (id) => {
+  return users["users_list"].find((user) => user["id"] === id);
+};
+
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
@@ -52,6 +59,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
